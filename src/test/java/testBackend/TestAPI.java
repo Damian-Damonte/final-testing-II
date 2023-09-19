21874053f1cdab1d7e94e8836aeb97234d86772e
damiandamonte <damontedamian@gmail.com>
2023-09-19 20:19:55 -0300

    final terminado. Finalizo parte de backend

--- src/test/java/testBackend/TestAPI.java
+++ src/test/java/testBackend/TestAPI.java
@@ -1,17 +1,14 @@
 package testBackend;
 
-import com.google.gson.JsonObject;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+import testFrontend.reports.ExtentFactory;
 
-import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,95 +19,167 @@ public class TestAPI {
     private final static String USERNAME = "juanfer1";
     private final static String PASSWORD = "123456";
     private static String userId;
-    private static String cuentaId;
+    private static String cuentaId1;
+    private static String cuentaId2;
+    private static ExtentReports extent;
+    static ExtentSparkReporter info = new ExtentSparkReporter("target/reporte_test_back.html");
+
+    @BeforeAll
+    public static void setupDriver() {
+        extent = ExtentFactory.getInstance();
+        extent.attachReporter(info);
+
+    }
+    @AfterAll
+    public static void reporte() {
+        extent.flush();
+    }
 
 
     @Test
     @Order(1)
+    @Tag("testBack")
     public void register() {
-        given()
-                .baseUri(BASE_URL)
-        .when()
-                .get("/register.htm")
-        .then()
-                .statusCode(200);
+        ExtentTest test = extent.createTest("Register");
+        Response response = given().baseUri(BASE_URL).when().get("/register.htm");
+
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Página register devuelve status 200 corectamente");
+        } else {
+            test.log(Status.FAIL, "Página register no devuelve status 200");
+        }
+        assertEquals(200, response.getStatusCode());
     }
 
     @Test
     @Order(2)
+    @Tag("testBack")
     public void login() {
+        ExtentTest test = extent.createTest("Login");
         Response response = when().get(BASE_URL + "/services/bank/login/" + USERNAME + "/" + PASSWORD);
         XmlPath xmlPath = new XmlPath(response.getBody().asString());
-        String id = xmlPath.getString("customer.id");
-        System.out.println("userId: " + id);
-        userId = id;
-        assertEquals(200, response.getStatusCode());
-        assertNotNull(id);
-        given().cookie("JSESSIONID", "6067A3F694EB070C87488A9750DD6249").
-        when().get(BASE_URL + "/services/bank/login/" + USERNAME + "/" + PASSWORD).then().log().everything();
-    }
+        userId = xmlPath.getString("customer.id");
 
-    @Test
-    public void login2() {
-        given().formParam("username", "juanfer1").formParam("password", "123456").
-                when().get(BASE_URL + "/login.htm").then().log().everything();
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Endpoint login devuelve status 200 correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint login no devuelve status 200");
+        }
+
+        if(userId != null) {
+            test.log(Status.PASS, "Id de usuario obtenido correctamente");
+        } else {
+            test.log(Status.FAIL, "No se pudo obtener el id del usuario");
+        }
+
+        assertEquals(200, response.getStatusCode());
+        assertNotNull(userId);
     }
 
     @Test
     @Order(3)
+    @Tag("testBack")
     public void getCuentas() {
-        System.out.println("userid: " + userId);
+        ExtentTest test = extent.createTest("Obtener cuentas");
         Response response = when().get(BASE_URL + "/services/bank/customers/" + userId + "/accounts");
         XmlPath xmlPath = new XmlPath(response.getBody().asString());
-        cuentaId = xmlPath.getString("accounts.account[0].id");
+        cuentaId1 = xmlPath.getString("accounts.account[0].id");
+
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Endpoint obtener cuentas devuelve status 200 correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint obtener cuentas no devuelve status 200");
+        }
+
+        if(cuentaId1 != null) {
+            test.log(Status.PASS, "Id de la cuenta del usuario obtenido correctamente");
+        } else {
+            test.log(Status.FAIL, "No se pudo obtener el id de la cuenta del usuario");
+        }
+
         assertEquals(200, response.getStatusCode());
+        assertNotNull(cuentaId1);
     }
 
     @Test
     @Order(4)
+    @Tag("testBack")
     public void nuevaCuenta() {
-        System.out.println("idCuenta: " + cuentaId);
-        given()
-                .baseUri(BASE_URL)
-        .when()
-                .post("/services/bank/createAccount?customerId=" + userId +"&newAccountType=1&fromAccountId=" + cuentaId)
-        .then()
-                .log().everything()
-                .statusCode(200);
+        ExtentTest test = extent.createTest("Abir nueva cuenta");
+        Response response = given().baseUri(BASE_URL).when()
+                .post("/services/bank/createAccount?customerId=" + userId +"&newAccountType=1&fromAccountId=" + cuentaId1);
+
+        XmlPath xmlPath = new XmlPath(response.getBody().asString());
+        cuentaId2 = xmlPath.getString("account.id");
+
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Endpoint abrir cuenta devuelve status 200 correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint abrir cuenta no devuelve status 200");
+        }
+
+        if(cuentaId2 != null) {
+            test.log(Status.PASS, "Cuenta creada correctamente");
+        } else {
+            test.log(Status.FAIL, "Error al obtener id de cuenta creada");
+        }
+
+        assertEquals(200, response.getStatusCode());
+        assertNotNull(cuentaId2);
     }
 
     @Test
     @Order(5)
-    public void resumenCuenta() {
-        given()
-                .baseUri(BASE_URL)
-                .cookie("JSESSIONID", "6067A3F694EB070C87488A9750DD6249")
-                .when()
-                .get("/overview.htm")
-                .then()
-                .log().everything()
-                .statusCode(200);
+    @Tag("testBack")
+    public void trasferir() {
+        ExtentTest test = extent.createTest("Transferir");
+        Response response = given().baseUri(BASE_URL).when()
+                .post("/services/bank/transfer?fromAccountId=" + cuentaId1 +"&toAccountId=" + cuentaId2 +"&amount=250");
+
+        String mensajeResponse = response.getBody().asString();
+        String mensajeEsperado = "Successfully transferred $250 from account #" + cuentaId1 + " to account #" + cuentaId2;
+
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Endpoint transferir devuelve status 200 correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint transferir no devuelve status 200");
+        }
+
+        if(mensajeEsperado.equals(mensajeResponse)) {
+            test.log(Status.PASS, "Transferencia realizada correctamente");
+        } else {
+            test.log(Status.FAIL, "Error al realizar transferencia");
+        }
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(mensajeEsperado, mensajeResponse);
     }
-//
-//    @Test
-//    public void descargarFondo() {
-//        given()
-//                .baseUri(BASE_URL)
-//                .when()
-//                .post("/services_proxy/bank/transfer?fromAccountId=13566&toAccountId=13677&amount=200")
-//                .then()
-//                .statusCode(200)
-//                .log().everything();
-//    }
-//
-//    @Test
-//    public void actividadCuenta() {
-//        given()
-//                .baseUri(BASE_URL)
-//                .when()
-//                .get("/services_proxy/bank/accounts/13566/transactions/month/All/type/All")
-//                .then()
-////                .statusCode(200)
-//                .log().everything();
-//    }
+
+    @Test
+    @Order(6)
+    @Tag("testBack")
+    public void actividadCuenta() {
+        ExtentTest test = extent.createTest("Actividad cuenta");
+
+        Response response = given().baseUri(BASE_URL).when()
+                .get("/services/bank/accounts/" + cuentaId1 +"/transactions/month/All/type/All");
+
+        String responseBody = response.getBody().asString();
+
+        if(response.getStatusCode() == 200) {
+            test.log(Status.PASS, "Endpoint actividad cuenta devuelve status 200 correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint actividad cuenta no devuelve status 200");
+        }
+
+        if(responseBody != null && !responseBody.isEmpty()) {
+            test.log(Status.PASS, "Endpoint actividad cuenta devuelve informacion correctamente");
+        } else {
+            test.log(Status.FAIL, "Endpoint actividad cuenta no devuelve informacion");
+        }
+
+        assertEquals(200, response.getStatusCode());
+        assertNotNull(responseBody);
+        assertNotEquals("", responseBody);
+    }
 }
